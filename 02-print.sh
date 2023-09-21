59bd22b2f2099690334674a087826554c0822bc2
Arpana <muriki.arpana@gmail.com>
2023-09-21 17:17:31 +0530

    working with colors

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,9 @@ echo installing nginx
 echo Starting nginx service
 
 echo -e "\e[31mHello In Red Color\e[0m"
+echo -e "\e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Color\e[0m"
+echo -e "\e[35mHello in Megenta Color\e[0m"
+echo -e "\e[36mHello in Cyan Color\e[0m"
+
